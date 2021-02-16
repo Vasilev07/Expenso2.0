5f0484ad02db0410ef0a64506d3d5621dc8407b6
Vasilev07 <georgevasilev07@gmail.com>
2021-02-16 23:44:32 +0200

    added initial enum/status

--- BE/src/routes/expense.ts
+++ BE/src/routes/expense.ts
@@ -1,8 +1,10 @@
 import { NextFunction } from "express";
+import { Request, Response } from "express";
+import { Status } from "../enums/status.enum";
 
 export const init = (app: any): void => {
-    app.get('/expense', (request: Request, response: any, next: NextFunction): void => {
+    app.get('/expense', (request: Request, response: Response, next: NextFunction): void => {
         // response.send('Hello Georgi!');
-        response.json('data');
+        response.status(Status.OK).send({test: 'a'});
     });
 };
