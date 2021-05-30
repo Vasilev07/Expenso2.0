41878f4d5f1cd54e642db50c0c1e8dee294d3128
Vasilev07 <georgevasilev07@gmail.com>
2021-05-31 00:56:50 +0300

    added transactioon screen

--- FE/expenso2.0/src/app/spendings/transactions/transaction.page.ts
+++ FE/expenso2.0/src/app/spendings/transactions/transaction.page.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   templateUrl: './transaction.page.html'
 })
-export class TransactionPage implements OnInit {
+export class TransactionPage {
+  public isExpense: boolean;
+  public date = new Date().toLocaleDateString();
 
-  public ngOnInit(): void {
-    console.log('transaction');
-
+  public expenseToggleSwitched(value: CustomEvent): void {
+    this.isExpense = value.detail.value === 'expense';
   }
-
 }
