fbdee49bc71feae471053f8c5d7b23b65f26e1bf
Vasilev07 <georgevasilev07@gmail.com>
2021-06-18 23:36:54 +0300

    refactored repeating code

--- BE/src/routes/transactions.ts
+++ BE/src/routes/transactions.ts
@@ -4,6 +4,44 @@ import { ObjectId } from 'mongodb';
 import { transactionSpendingsAggregation } from '../repositories/spendings';
 import { addExpense, addIncome, getAllTransactions, getSpendings, removeTransaction, updateTransaction } from '../services/transaction.service';
 
+const addTransaction = async (collection:any, transaction: any, token: any, currentTransactionId: any) => {
+
+    const date = new Date(transaction.date);
+
+    const decodedToken = decode(token.split(' ')[1]) as any;
+    const userId = decodedToken.userId;
+
+    const isExpense = transaction.isExpense;
+
+    const filter = {
+      userId: new ObjectId(userId),
+      date: `${date.getMonth() + 1}-${date.getFullYear()}`
+    };
+
+    const transactionToSave = {
+      _id: currentTransactionId ? new ObjectId(currentTransactionId) : new ObjectId(),
+      date: date.toString(),
+      amount: parseFloat(transaction.amount),
+      category: {
+        categoryId: new ObjectId(transaction.category.categoryId),
+        name: transaction.category.name,
+        icon: transaction.category.icon
+      }
+    };
+
+    isExpense ?
+      await addExpense(
+        collection,
+        filter,
+        transactionToSave
+      ):
+      await addIncome(
+        collection,
+        filter,
+        transactionToSave
+      );
+}
+
 export const init = (app: any, collection: any): void => {
   app.get('/transaction/spendings', async (request: Request, response: Response, next: NextFunction): Promise<any> => {
     const transaction = await getSpendings(collection, transactionSpendingsAggregation);
@@ -18,43 +56,7 @@ export const init = (app: any, collection: any): void => {
   });
 
   app.post('/transaction', async (request: Request, response: Response, next: NextFunction): Promise<any> => {
-    const transaction = request.body;
-    const date = new Date(transaction.date);
-
-    const token = request.headers.authorization as any;
-
-    const decodedToken = decode(token.split(' ')[1]) as any;
-    const userId = decodedToken.userId;
-
-    const isExpense = transaction.isExpense;
-
-    const filter = {
-      userId: new ObjectId(userId),
-      date: `${date.getMonth() + 1}-${date.getFullYear()}`
-    };
-
-    const transactionToSave = {
-      _id: new ObjectId(),
-      date: date.toString(),
-      amount: parseFloat(transaction.amount),
-      category: {
-        categoryId: new ObjectId(transaction.category.categoryId),
-        name: transaction.category.name,
-        icon: transaction.category.icon
-      }
-    };
-
-    isExpense ?
-      await addExpense(
-        collection,
-        filter,
-        transactionToSave
-      ):
-      await addIncome(
-        collection,
-        filter,
-        transactionToSave
-      )
+    await addTransaction(collection, request.body, request.headers.authorization, null);
 
     return response.status(200).json();
   });
@@ -67,47 +69,9 @@ export const init = (app: any, collection: any): void => {
     const isExpense = transaction.isExpense;
 
     const removeFrom = isExpense ? 'expenses': 'incomes';
-    console.log(removeFrom);
-    console.log(transactionId);
-    console.log(currentTransactionId);
 
     await removeTransaction(collection, removeFrom, transactionId, currentTransactionId);
-
-    // extract this sometimes in the future
-    const date = new Date(transaction.date);
-
-    const token = request.headers.authorization as any;
-
-    const decodedToken = decode(token.split(' ')[1]) as any;
-    const userId = decodedToken.userId;
-
-    const filter = {
-      userId: new ObjectId(userId),
-      date: `${date.getMonth() + 1}-${date.getFullYear()}`
-    };
-
-    const transactionToSave = {
-      _id: new ObjectId(currentTransactionId),
-      date: date.toString(),
-      amount: parseFloat(transaction.amount),
-      category: {
-        categoryId: new ObjectId(transaction.category.categoryId),
-        name: transaction.category.name,
-        icon: transaction.category.icon
-      }
-    };
-
-    isExpense ?
-      await addExpense(
-        collection,
-        filter,
-        transactionToSave
-      ):
-      await addIncome(
-        collection,
-        filter,
-        transactionToSave
-      )
+    await addTransaction(collection, request.body, request.headers.authorization, currentTransactionId);
 
     return response.status(200).json();
   });
