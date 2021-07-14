ec4103518b136315fb7cb6a4e2c7a918f6dac7de
Vasilev07 <georgevasilev07@gmail.com>
2021-07-14 23:18:51 +0300

    added test for tabs page

--- FE/expenso2.0/src/app/tabs/tabs.page.spec.ts
+++ FE/expenso2.0/src/app/tabs/tabs.page.spec.ts
@@ -1,8 +1,23 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
-import { TabsPage } from './tabs.page';
+import { Store } from "@ngrx/store";
+import { TabsPage } from "./tabs.page";
 
 describe('TabsPage', () => {
+    let sut: Partial<TabsPage>;
+    let store: Store;
 
+    beforeEach(() => {
+        store = {
+            dispatch: jest.fn(),
+        } as any;
+
+        sut = new TabsPage(store);
+    });
+
+    describe('ngOnInit()', () => {
+        it('should call store.discpatch', () => {
+            sut.ngOnInit();
+
+            expect(store.dispatch).toHaveBeenCalled();
+        });
+    });
 });
