97bf0f083c26b223c1747a09d8c1cfb9b0e38295
Vasilev07 <georgevasilev07@gmail.com>
2021-07-21 23:09:56 +0300

    added tests for users service

---
+++ FE/expenso2.0/src/app/services/users.service.spec.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from "@angular/common/http";
+import { StorageService } from "./storage.service";
+import { UsersService } from "./users.service";
+
+describe('UsersService', () => {
+    let sut: UsersService;
+    let httpService: HttpClient;
+    let storageService: StorageService;
+
+    beforeEach(() => {
+        httpService = {
+            post: jest.fn()
+        } as any;
+        storageService = {
+            set: jest.fn(),
+            get: jest.fn()
+        } as any;
+
+        sut = new UsersService(httpService, storageService);
+    });
+
+    describe('login()', () => {
+        it('should call post', () => {
+            sut.login({} as any);
+
+            expect(httpService.post).toHaveBeenCalled();
+        });
+    });
+
+    describe('storeToken()', () => {
+        it('should call set', () => {
+            sut.storeToken({} as any);
+
+            expect(storageService.set).toHaveBeenCalled();
+        });
+    });
+
+    describe('getToken()', () => {
+        it('should call set', () => {
+            sut.getToken();
+
+            expect(storageService.get).toHaveBeenCalled();
+        });
+    });
+
+    describe('updateUserPreferences()', () => {
+        it('should call post', () => {
+            sut.updateUserPreferences({} as any);
+
+            expect(httpService.post).toHaveBeenCalled();
+        });
+    });
+
+});
