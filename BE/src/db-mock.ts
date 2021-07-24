e5deb47d4fb9c6bd008186861910f2067c46baf0
Vasilev07 <georgevasilev07@gmail.com>
2021-07-24 12:53:19 +0300

    fixed mongodb jest config

---
+++ BE/src/db-mock.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+export const mongoDbMockConnect = async () => {
+    let connection;
+    let db;
+
+    connection = await MongoClient.connect('mongodb://localhost:27017/test', {
+        useNewUrlParser: true,
+    });
+    db = await connection.db('test');
+
+    return {
+        db,
+        connection
+    }
+}
