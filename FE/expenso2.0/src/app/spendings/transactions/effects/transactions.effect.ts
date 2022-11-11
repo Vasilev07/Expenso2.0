96df3e0ee4455427dcf86f2b034457f20b6a8e79
Vasilev07 <georgevasilev07@gmail.com>
2022-11-11 17:31:24 +0200

    fixed files

--- FE/expenso2.0/src/app/spendings/transactions/effects/transactions.effect.ts
+++ FE/expenso2.0/src/app/spendings/transactions/effects/transactions.effect.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { EMPTY } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
-import { TransactionService } from "../services/transaction.service";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { TransactionService } from '../services/transaction.service';
 
 @Injectable()
 export class TransactionEffects {
 
     constructor(private readonly actions$: Actions,
-        private readonly transactionService: TransactionService) {
+                private readonly transactionService: TransactionService) {
     }
 
     $createTransaction = createEffect((): any => {
@@ -25,7 +25,9 @@ export class TransactionEffects {
     $retrieveExpences = createEffect((): any => {
         return this.actions$.pipe(
             ofType('[Transaction Spendings] Retrieve Transaction Spendings'),
-            switchMap((data: { type: string, date: string, wholeYearSelected: boolean }) => this.getAllTransactions(data.date, data.wholeYearSelected))
+            switchMap((data: { type: string, date: string, wholeYearSelected: boolean }) =>
+                this.getAllTransactions(data.date, data.wholeYearSelected)
+            )
         );
     });
 
