4e6aafef3632c977d0f99d9856dafc81c44bc7d3
Vasilev07 <georgevasilev07@gmail.com>
2023-01-18 13:53:04 +0200

    Fix cloud infrastructure

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -4,7 +4,8 @@ import { startServer } from './server';
 startServer()
     .then((app: any) => {
         const hostname = '0.0.0.0';
-        app.listen(port, hostname, () => console.log(`Listening on ${hostname}:${port}`));
+        const server = app.listen(port, hostname, () => console.log(`Listening on ${hostname}:${port}`));
+        server.keepAliveTimeout = 65000;
         console.log('Server successfully started');
     })
     .catch((error: Error) => {
