a11d71b32de0c6c91b949eeabe79229a75fbcd05
Vasilev07 <georgevasilev07@gmail.com>
2023-01-22 21:10:28 +0200

    Fix infra

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -6,14 +6,14 @@ import { DeploymentUi } from '../lib/deployment-ui';
 
 const app = new cdk.App();
 
-// const server = new DeploymentServer(app, 'DeploymentStack', {
-//     env: {
-//         account: process.env.CDK_DEFAULT_ACCOUNT,
-//         region: process.env.CDK_DEFAULT_REGION
-//     }
-// });
+const server = new DeploymentServer(app, 'DeploymentStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
 
-new DeploymentUi(app, 'DeploymentStackPROD', 'server.uri',
+new DeploymentUi(app, 'DeploymentStackPROD', server.uri,
     {
         env: {
             account: process.env.CDK_DEFAULT_ACCOUNT,
