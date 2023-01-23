3312eb92527c1e71d7128b2507dea8c90e40f154
Vasilev07 <georgevasilev07@gmail.com>
2023-01-23 17:02:08 +0200

    Fix aws cdk infra

--- FE/expenso2.0/src/app/app.module.ts
+++ FE/expenso2.0/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { InjectionToken, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
